3663f47c8552e7b4470f5a92618dafaf62941f31
sujan khatri <sujansince2059@gmail.com>
2024-03-12 12:27:55 +0545

    added logos to hero

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -4,31 +4,16 @@ import logo from "public/assets/mainlogo.png";
 
 import handshake from "public/assets/handshakee.png";
 import grace from "public/assets/gracelogo.jpeg";
-import { useState, useEffect } from "react";
+
 import Typewriter from "./Typewriter";
 
 export const HeroSection = () => {
-  const [days, setDays] = useState("00");
-  const [hours, setHours] = useState("00");
-  const [minutes, setMinutes] = useState("00");
-
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://apply.devfolio.co/v2/sdk.js";
-    script.async = true;
-    script.defer = true;
-    document.body.appendChild(script);
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
-
   return (
     <section
       id="Home"
-      className="min-h-screen flex lg:flex-row items-center flex-col py-10 md:py-2 justify-between"
+      className="md:min-h-screen flex lg:flex-row flex-col-reverse md:items-center md:flex-col py-10 md:py-2 md:justify-between"
     >
-      <div className="w-[50%] flex  justify-between flex-col">
+      <div className="md:w-[50%] w-full  flex  justify-between flex-col">
         <div className="text-white md:px-16 px-10 flex flex-col gap-3">
           <h1 className="md:text-7xl text-5xl font-extrabold">
             Join Gracathon
@@ -51,7 +36,7 @@ export const HeroSection = () => {
           </p>
           <div className="pt-6 z-10 flex gap-x-4 flex-col gap-y-4">
             <a
-              href="https://discord.gg/H7tcpbv7Vq"
+              href=""
               target="_blank"
               rel="noopener noreferrer"
               className="flex w-80 items-center justify-center gap-2 rounded-md bg-indigo-500 py-2.5 text-white"
@@ -59,7 +44,7 @@ export const HeroSection = () => {
               <span className="font-bold">Register now</span>
             </a>
             <a
-              href="dicord link"
+              href="https://discord.gg/Vp4ujUJfKy"
               target="_blank"
               rel="noopener noreferrer"
               className="flex w-80 items-center justify-center gap-2 rounded-md bg-indigo-500 py-2.5 text-white"
@@ -71,27 +56,46 @@ export const HeroSection = () => {
         </div>
       </div>
 
-      <div className="flex justify-between  gap-1 ">
+      <div className="md:flex hidden justify-between  gap-1 pr-8 ">
+        <a href="https://csitabmc.com/" target="_blank">
+          <Image
+            className="rounded-full "
+            src={logo}
+            alt="association logo"
+            height={100}
+            width={180}
+          />
+        </a>
+        <Image src={handshake} alt="association logo" height={50} width={180} />
+        <a href="https://facebook.com" target="_blank">
+          <Image
+            className="rounded-full"
+            src={grace}
+            alt="grace logo"
+            width={200}
+            height={100}
+          />
+        </a>
+      </div>
+
+      <div className="flex md:hidden justify-between  gap-1 md:px-16 px-10 my-10 ">
         <Image
           className="rounded-full "
           src={logo}
           alt="association logo"
           height={100}
-          width={180}
+          width={100}
         />
-        <Image src={handshake} alt="association logo" height={50} width={180} />
+        <Image src={handshake} alt="association logo" height={50} width={100} />
 
         <Image
           className="rounded-full"
           src={grace}
           alt="association logo"
-          width={200}
-          height={100}
+          width={100}
+          height={150}
         />
       </div>
-      {/* <div className="px-16">
-        <Image src={Heroimg} alt="heroimg" />
-      </div> */}
     </section>
   );
 };
