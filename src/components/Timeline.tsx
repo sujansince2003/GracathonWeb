30c1d9193687d8194e071f492ace90cb3d04f930
sujan khatri <sujansince2059@gmail.com>
2024-03-12 12:28:28 +0545

    timeline stlying

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -4,7 +4,7 @@ export function Timeline() {
   return (
     <section
       id="Timeline"
-      className="py-20 w-full px-0 md:px-10 bg-[url('/assets/background.png')] bg-contain flex flex-col justify-center items-center"
+      className="py-20 w-full px-6 md:px-10 bg-[url('/assets/background.png')] bg-contain flex flex-col justify-center items-center"
     >
       <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"></hr>
       <div className="mb-20 text-center text-4xl font-semibold sm:text-5xl font-noto-sans bg-clip-text  bg-gradient-to-b from-white to-[#AAAAAA]">
